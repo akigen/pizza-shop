220abb7902a45f3b9f05c743780a6371e3222c5a
genakimov <ga.spb7@gmail.com>
2020-08-15 00:23:54 +0300

    repair some bugs, add bootstrap to project

--- pizzashopproject/urls.py
+++ pizzashopproject/urls.py
@@ -1,4 +1,4 @@
-"""pizzashopproject1 URL Configuration
+"""pizzashopproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
