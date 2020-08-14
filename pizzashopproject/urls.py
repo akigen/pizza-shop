1b38af8406e5df4d7a6c6d90376a9f0b75785a6c
genakimov <ga.spb7@gmail.com>
2020-08-14 23:47:25 +0300

    move directories. Add general settings, create pizzashopapp, add home page.

--- pizzashopproject/urls.py
+++ pizzashopproject/urls.py
@@ -1,4 +1,4 @@
-"""pizzashopproject URL Configuration
+"""pizzashopproject1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pizzashopapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
