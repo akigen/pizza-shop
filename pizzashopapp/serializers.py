966002e4ababbb0b56ee5724e6850cb7599f0948
genakimov <ga.spb7@gmail.com>
2020-08-30 21:47:49 +0300

    add api for pizza

--- pizzashopapp/serializers.py
+++ pizzashopapp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from pizzashopapp.models import PizzaShop
+from pizzashopapp.models import PizzaShop, Pizza
 
 
 class PizzaShopSerializer(serializers.ModelSerializer):
@@ -13,3 +13,16 @@ class PizzaShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = PizzaShop
         fields = ('id', 'name', 'phone', 'address', 'logo')
+
+
+class PizzaSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, pizza):
+        request = self.context.get('request')
+        image_url = pizza.image.url
+        return request.build_absolute_uri(image_url)
+
+    class Meta:
+        model = Pizza
+        fields = ('name', 'short_description', 'image', 'price')
--- pizzashopproject/urls.py
+++ pizzashopproject/urls.py
@@ -24,20 +24,22 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('pizzashop/sign-in', LoginView.as_view(template_name='pizzashop/sign-in.html'),
-         name='pizzashop-sign-in'),
 
-    path('pizzashop/sign-in', LogoutView.as_view(next_page='/'),
-         name='pizzashop-sign-out'),
-
+    path('pizzashop/sign-in/', LoginView.as_view(template_name='pizzashop/sign_in.html'),
+        name='pizzashop-sign-in'),
+    path('pizzashop/sign-out', LogoutView.as_view(next_page='/'),
+        name='pizzashop-sign-out'),
     path('pizzashop/', views.pizzashop_home, name='pizzashop-home'),
 
     path('pizzashop/sign-up', views.pizzashop_sign_up, name='pizzashop-sign-up'),
 
-    path('pizzashop/account', views.pizzashop_account, name='pizzashop-account'),
-    path('pizzashop/pizza', views.pizzashop_pizza, name='pizzashop-pizza'),
-    path('pizzashop/pizza/add', views.pizzashop_add_pizza, name='pizzashop-add-pizza'),
+    path('pizzashop/account/', views.pizzashop_account, name='pizzashop-account'),
+    path('pizzashop/pizza/', views.pizzashop_pizza, name='pizzashop-pizza'),
+    path('pizzashop/pizza/add/', views.pizzashop_add_pizza, name='pizzashop-add-pizza'),
     path('pizzashop/pizza/edit/<int:pizza_id>', views.pizzashop_edit_pizza, name='pizzashop-edit-pizza'),
-    path('api/client/pizzashops/', apis.client_get_pizzashops),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # APIS
+    path('api/client/pizzashops/', apis.client_get_pizzashops),
+    path('api/client/pizzas/<int:pizzashop_id>/', apis.client_get_pizzas),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
